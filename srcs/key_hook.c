e2ffade45e37a9fc4d3afc79551b4832f6205a11
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-02-18 18:31:55 +0900

    矢印キーで角度変更

--- srcs/key_hook.c
+++ srcs/key_hook.c
@@ -6,7 +6,7 @@
 /*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:25:12 by kitaoryoma        #+#    #+#             */
-/*   Updated: 2025/02/18 18:08:29 by kitaoryoma       ###   ########.fr       */
+/*   Updated: 2025/02/18 18:12:50 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,13 @@ int	key_hook(int keycode, t_data *data)
 		data->player.position.x -= PACE;
 	else if (keycode == D)
 		data->player.position.x += PACE;
+	else if (keycode == LA)
+		data->player.angle += TURNANGLE;
+	else if (keycode == RA)
+		data->player.angle -= TURNANGLE;
+	// 回転によって向きが変わったのでdirectionを再計算
+	data->player.direction.x = cos(data->player.angle);
+	data->player.direction.y = sin(data->player.angle);
 	PRINTF("player position: %f, %f\n", data->player.position.x, data->player.position.y);
 	ft_put_floor_and_ceiling(data);
 	ft_draw_wall(data);
