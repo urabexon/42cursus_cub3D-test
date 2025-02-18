547c923ee52034dc83e9ada909dfece2784c4f36
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-02-18 19:27:42 +0900

    壁にのめり込まないようにした

--- srcs/key_hook.c
+++ srcs/key_hook.c
@@ -6,14 +6,28 @@
 /*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:25:12 by kitaoryoma        #+#    #+#             */
-/*   Updated: 2025/02/18 18:35:04 by kitaoryoma       ###   ########.fr       */
+/*   Updated: 2025/02/18 19:22:49 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+int is_in_wall(t_data *data, t_vector position)
+{
+	int x;
+	int y;
+
+	x = position.x / PX;
+	y = position.y / PX;
+	if (data->map[data->rows - 1 - y][x] == '1')
+		return (1);
+	return (0);
+}
+
 int	key_hook(int keycode, t_data *data)
 {
+	// 移動前の座標を保持
+	t_vector old_position = data->player.position;
 	if (keycode == ESC)
 		exit_game(data);
 	if (keycode == W)
@@ -40,6 +54,8 @@ int	key_hook(int keycode, t_data *data)
 		data->player.angle += TURNANGLE;
 	else if (keycode == RA)
 		data->player.angle -= TURNANGLE;
+	if (is_in_wall(data, data->player.position))
+		data->player.position = old_position;
 	// 回転によって向きが変わったのでdirectionを再計算
 	data->player.direction.x = cos(data->player.angle);
 	data->player.direction.y = sin(data->player.angle);
