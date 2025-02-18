8d0434ac20eae9ca5e14764080a9db92d7b60667
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-02-18 22:30:05 +0900

    ミニマップの色の説明コメント

--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 19:37:47 by kitaoryoma        #+#    #+#             */
-/*   Updated: 2025/02/18 20:27:23 by kitaoryoma       ###   ########.fr       */
+/*   Updated: 2025/02/18 22:29:38 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,11 @@ void	ft_draw_minimap(t_data *data)
 			pos.y = data->player.position.y + (data->player.direction.y * y - data->player.direction.x * x) * 4;
 			if ((int)pos.y / PX < 0 || (int)pos.y / PX >= data->rows
 				|| (int)pos.x / PX < 0 || (int)pos.x / PX >= data->columns)
-				color = 0x000000;
+				color = 0x101010;//マップ範囲外（黒色）
 			else if (data->map[data->rows - 1 - (int)pos.y / PX][(int)pos.x / PX] == '1')
-				color = 0x696969;
+				color = 0x696969;//壁（灰色）
 			else
-				color = 0xf0f8ff;
+				color = 0xf0f8ff;//通路（薄い水色）
 			*(unsigned int *)(data->graphic.minimap_img.addr + (MINIMAP_SIZE / 2 - y - 1) * data->graphic.minimap_img.line_size
 				+ (x + MINIMAP_SIZE / 2) * (data->graphic.minimap_img.bits_per_pixel / 8)) = color;
 			y++;
